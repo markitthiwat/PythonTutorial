6d5fbf968de66878a31ee3abca4740a7f07cffa2
markitthiwat <mark.itthiwat@gmail.com>
2022-10-27 01:05:58 +0700

    Variables and DataTypes

---
+++ variableanddatatype.py
@@ -0,0 +1,12 @@
+name = "Katshashin"
+passion = "Harley Davidson"
+url = "https://foundation.app/@katshashin"
+number = "1"
+
+
+print("My name is " + name)
+print("I like " + passion + " in a Vicla style")
+print("My Foundation name is " + name)
+print("I create NFT because my passion is " + passion)
+print("You can follow me at " + url)
+print("I have " + number + " " + passion)
